978465ee6bb48ea01952d81b7a7a7c1ee10c6aa2
AgRaRA <53420901+agrara@users.noreply.github.com>
2024-06-08 18:51:44 +0300

    Bug fix

--- Task1.java
+++ Task1.java
@@ -11,7 +11,6 @@ public class Task1 {
         int n = Integer.parseInt(sc.nextLine()); // represents the initial count
         int m = Integer.parseInt(sc.nextLine()); // represents the increase of the count
         int h = Integer.parseInt(sc.nextLine()); // represents the number of hours
-        int totalOrcs = n;
 
          int totalOrcs = h * n + m * h * (h - 1) / 2;
 
